184260d79ef859cf6b3fb97adb6a735afed8e452
unknown <reututg@gmail.com>
2019-05-05 18:26:58 +0300

    added rented section + movie details page

--- reflix/src/components/Movie.js
+++ reflix/src/components/Movie.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+// import MovieDetail from './MovieDetail';
+
 
 class Movie extends Component {
+
+  isRented = (event) => {
+    event.preventDefault()
+    this.props.isRented(this.props.movie.id)
+  }
+
   render() {
+    let movie = this.props.movie
+
     return (
-      <div>
-        
+        <Link to={`/catalog/${movie.id}`}> 
+      <div className="movie" id={movie.id} style={{ backgroundImage: `url(${movie.img})` }}>
+        {movie.isRented ?
+          <i className="fas fa-minus-circle" onClick={this.isRented}></i> :
+          <i className="fas fa-plus-circle" onClick={this.isRented}></i>}
+      {/* <MovieDetail key={movie.id} movie={this.props.movie}/> */}
       </div>
+          </Link>
+
     )
   }
 }
---
+++ reflix/src/components/MovieDetail.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class MovieDetail extends Component {
+    render() {
+        let movieID = this.props.match.params.id
+        let movies = this.props.state.movies
+        let movie = movies.find(movie=> movie.id == movieID)
+        console.log(this.props.match.params)
+        console.log(movies)
+        return (
+            <div className="movie-details">
+                hello
+            
+        <h2>{`${movie.title} (${movie.year})`}</h2>
+
+            <img src={movie.img} alt={movie.title} />
+
+            <p>{movie.descrShort}</p> 
+            
+            </div>
+        )
+    }
+}
+
+export default MovieDetail
